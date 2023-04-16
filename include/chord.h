ba3c610aae563b41a44b688f32b4e1c0f722dea7
Stephen DeBoy <sdeboy@terpmail.umd.edu>
2023-04-15 20:41:17 -0400

    updated chord.h

--- include/chord.h
+++ include/chord.h
@@ -23,3 +23,24 @@ typedef struct Message
  * you how to properly print out an unsigned 64 bit integer.
  */
 void printKey(uint64_t key);
+void stabilize();
+void fix_fingers();
+void check_predecessor();
+void create();
+void join();
+void handle_message(ChordMessage *msg);
+
+// helper functions
+void add_connection(struct pollfd **pfds, int fd,
+                    int *p_cons, int *p_size);
+void remove_connection(struct pollfd **pfds, int fd,
+		       int* p_cons);
+void update_chord(struct chord_arguments *args,
+                  struct timespec *curr_time,
+		  struct timespec *last_stab,
+                  struct timespec *last_ff,
+		  struct timespec *last_cp);
+double time_diff(struct timespec *t1,
+		 struct timespec *t2);
+double deci_to_sec(int time);
+int init_socket(struct sockaddr_in *addr);
